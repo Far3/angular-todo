81a5055593f73afad56920d36be9c43a49dfd5ac
Franklyn Roth <franklyn.roth@markit.com>
2016-05-10 16:18:04 -0600

    Complete though scope and controllers

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
+//The OG
 //2nd parameter includes dependencies. Its empty so angular knows to create it.
-var app = angular.module("todoListApp", []);
+// angular.module("todoListApp", [])
+// .controller('mainCtrl', function($scope) {
+// 	$scope.helloWorld = function() {
+// 		console.log('This is the hello world controller function.');
+// 	};
 
-app.run(function($rootScope) {
-	$rootScope.name = 'You did it right';		
+// });
+
+
+
+var app = angular.module('todoListApp', []);
+app.controller('mainCtrl', function($scope) {
+	$scope.helloWorld = function() {
+		console.log('This is the hello world controller function.');
+	};
+
+});
+
+app.controller('coolCtrl', function($scope) {
+	$scope.whoAmI = function() {
+		console.log('We are in the coolCtrl');	
+	};
 });
--- js/hello.js
+++
@@ -1,7 +0,0 @@
-angular.module('todoListApp')
-.directive('helloWorld', function() {
-	return {
-		template: "Hello Directive",
-		restrict: "E" //only use this as an element
-	};
-});
