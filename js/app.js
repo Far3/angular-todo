33c987b587595a654cfa02a52fdc194451d322cf
Franklyn Roth <franklyn.roth@markit.com>
2016-05-12 14:37:02 -0600

    Completed basic todo app.

--- js/app.js
+++ js/app.js
@@ -9,18 +9,47 @@
 // });
 
 var app = angular.module('todoListApp', []);
-app.controller('mainCtrl', function($scope) {
-	$scope.helloWorld = function() {
-		console.log('You have saved');
+app.controller('mainCtrl', function ($scope, dataService) {
+	
+	$scope.addTodo = function() {
+		var todo = {name: "This is a new todo."};
+		$scope.todos.push(todo);
 	};
 	
-	$scope.todos = [
-		{"name": "Lift At ECC"},	
-		{"name": "Clean Room"},
-		{"name": "Order United MPE Card"},
-		{"name": "Make a post"},
-		{"name": "New Money Comedy"},
-		{"name": "King Soopers Run"}	
-	]
+	$scope.helloConsole = dataService.helloConsole;
 
+	dataService.getTodos(function(response) {
+		console.log(response.data);
+		$scope.todos = response.data;
+	});
+	
+	$scope.deleteTodo = function(todo, $index) {
+		dataService.deleteTodo(todo);	
+		$scope.todos.splice($index, 1);
+	};
+	
+	$scope.saveTodos = function(todo){
+		dataService.saveTodos(todo);	
+	};
+});
+
+app.service('dataService', function($http){
+		this.helloConsole = function() {
+			console.log('service is working');
+		};
+		
+		this.getTodos = function(callback){ 
+			$http.get('mock/todos.json')
+			.then(callback)
+			console.log(callback);
+		};
+		
+		this.deleteTodo = function(todo) {
+			console.log('The', todo.name, 'has been deleted');
+
+		};
+		
+		this.saveTodos = function(todo) {
+			console.log('The', todo.name, 'has been saved');
+		}
 });
